1271a055fa1a7935601accb453f9c3b634b1e4ed
dpsethi <dpsethi@hotmail.com>
2018-12-12 13:36:30 -0500

    next turn

--- createvm1.py
+++ createvm1.py
@@ -5,6 +5,7 @@ import requests
 # provides the backbone for our random string
 # generator and then we have to import the string
 # module.
+#github commit done 12/12/2018
 
 import random
 import string
